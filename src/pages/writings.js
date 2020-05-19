10e1061a783f568335a39548fc2a43d7772fc77c
FabioRosado <fabioglrosado@gmail.com>
2020-05-19 21:53:10 +0100

    Resize images

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -1,35 +1,20 @@
 import React, { Component } from "react"
-import { Link, graphql } from "gatsby"
-import Img from 'gatsby-image'
+import { graphql } from "gatsby"
 
 import Layout from '../components/layout'
+import WritingCard from '../components/blog'
 
 
 class Blog extends Component {
-
     render() {
         const posts = this.props.data.blog
         return(
             <Layout>
             <h1 className="section center-text white-text">Blog</h1>
                 <section className="page-container">
-                    {posts.nodes.map((post) => {
-                        return (
-                            <div className="post-card" key={post.frontmatter.title}>
-                            <Link to={post.frontmatter.slug}>
-                                <div className="description">
-                                    <p className="white-text small-font margin-bottom">{ post.frontmatter.excerpt }</p>
-                                    <span className="trans-button small-font">Read More <i className="fas fa-angle-double-right"></i></span>
-                                </div>
-                                <div className="card-title">
-                                    <span className="category"><i className={post.frontmatter.category_icon}></i> {post.frontmatter.categories}</span>
-                                    <h4 className="white-text">{post.frontmatter.title}</h4>
-                                </div>
-                                <Img className="post-image" fluid={post.frontmatter.image.childImageSharp.fluid} alt={post.frontmatter.title} />
-                            </Link>
-                        </div>
-                        )
-                    } )}
+                    {posts.nodes.map((post) => 
+                      <WritingCard post={post} key={post.frontmatter.title} />  
+                    )}
                 </section>
             </Layout>
         )
