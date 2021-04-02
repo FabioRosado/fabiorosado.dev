8d2401f48ef97167a1f31a1a80713087f6e2b8cf
FabioRosado <fabioglrosado@gmail.com>
2021-04-02 19:29:01 +0100

    Add scott's OG image serverless function

---
+++ src/components/utils.js
@@ -0,0 +1,14 @@
+const objectToQueryParams = obj => {
+    const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+    return "?" + params.join('&')
+}
+
+export const ogImageUrl = (author, website, title) => {
+    const params = {
+        author,
+        website,
+        image: `https://fabiorosado.dev/logo.svg`,
+        title
+    }
+    return `http://image-og-now.sh/og.jpg${objectToQueryParams(params)}`
+}
--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -7,6 +7,7 @@ import SEO from "../components/seo"
 import SimilarArticlesList from "../components/SimilarArticles/SimilarArticlesList"
 import Webmentions from "../components/webmentions/webmentionsComponent"
 import MentionsCounter from "../components/webmentions/counter"
+import { ogImageUrl } from "../components/utils"
 
 
 const Template = (props) => {
@@ -35,7 +36,7 @@ const Template = (props) => {
     const [totalMentions, setTotalMentions] = useState(0)
     const [show, setShow] = useState(false)
     const { frontmatter } = props.pageContext
-    
+
     const [postInformation] = data.allMdx.nodes.filter(post => {
         if (post.slug === frontmatter.slug) {
             return post
@@ -51,9 +52,9 @@ const Template = (props) => {
                 `https://webmention.io/api/count.json?target=https://fabiorosado.dev/blog/${frontmatter.slug}/`
             )
 
-        const { count } = await resp.json()
+            const { count } = await resp.json()
 
-        setTotalMentions(count)
+            setTotalMentions(count)
         }
         fetchTotalMentions()
     }, [frontmatter.slug])
@@ -61,15 +62,18 @@ const Template = (props) => {
 
     return (
         <Layout>
-            <SEO title={`FabioRosado | ${frontmatter.title}`} description={frontmatter.excerpt} />
+            <SEO
+                title={`FabioRosado | ${frontmatter.title}`}
+                description={frontmatter.excerpt}
+                image={ogImageUrl("FabioRosado", "fabiorosado.dev", frontmatter.title)} />
             <section className="blog-post">
                 <div className="post-header">
                     <h3 className="category-text">{frontmatter.categories}</h3>
                     <h1 className="white-text larger">{frontmatter.title}</h1>
                     <p className="white-text metadata">
-                        <span className="metadata-icons"><i className="far fa-user orange-text"/> {frontmatter.author || `FabioRosado`}</span>
-                        <span className="metadata-icons"><i className="far fa-clock orange-text"/> {date}</span>
-                        <span className="metadata-icons"><i className="far fa-eye orange-text"/> {timeToRead > 1 ? `${timeToRead} mins to read` : `${timeToRead} min to read`} </span>
+                        <span className="metadata-icons"><i className="far fa-user orange-text" /> {frontmatter.author || `FabioRosado`}</span>
+                        <span className="metadata-icons"><i className="far fa-clock orange-text" /> {date}</span>
+                        <span className="metadata-icons"><i className="far fa-eye orange-text" /> {timeToRead > 1 ? `${timeToRead} mins to read` : `${timeToRead} min to read`} </span>
                         <MentionsCounter postUrl={`https://fabiorosado.dev/blog/${frontmatter.slug}`} styles="metadata-icons" />
                     </p>
                 </div>
@@ -80,39 +84,39 @@ const Template = (props) => {
                             <h1 className="dark-text">{frontmatter.subtitle}</h1>
                             {props.children}
                         </div>
-                    <div className="webmentions-container">
-                        <h3>Webmentions</h3>
-                        <div>
-                            <MentionsCounter postUrl={`https://fabiorosado.dev/blog/${frontmatter.slug}`} styles="padding-bottom" />
-                        </div>
-                        <button 
-                            className="full-button" 
-                            aria-label="Show all webmentions"
-                            onClick={() => setShow(!show)}
-                        >
-                            Show {totalMentions} Mentions
+                        <div className="webmentions-container">
+                            <h3>Webmentions</h3>
+                            <div>
+                                <MentionsCounter postUrl={`https://fabiorosado.dev/blog/${frontmatter.slug}`} styles="padding-bottom" />
+                            </div>
+                            <button
+                                className="full-button"
+                                aria-label="Show all webmentions"
+                                onClick={() => setShow(!show)}
+                            >
+                                Show {totalMentions} Mentions
                         </button>
-                    </div>
+                        </div>
 
-                    <Webmentions slug={frontmatter.slug} show={`${show ? "visible" : "invisible"}`}/>
+                        <Webmentions slug={frontmatter.slug} show={`${show ? "visible" : "invisible"}`} />
 
                     </div>
                 </article>
-                </section>
-                {frontmatter.categories === "Projects" ? "" : <SimilarArticlesList categories={frontmatter.categories} tags={frontmatter.tags} currentArticlePath={frontmatter.slug} />}
-                
-                <section className="profile-section">
-                    <div className="h-card">
+            </section>
+            {frontmatter.categories === "Projects" ? "" : <SimilarArticlesList categories={frontmatter.categories} tags={frontmatter.tags} currentArticlePath={frontmatter.slug} />}
+
+            <section className="profile-section">
+                <div className="h-card">
                     <Img className="u-photo" fixed={data.fabiorosado.childImageSharp.fixed} alt="FabioRosado" />
-                        <a className="p-name u-url" href="https://fabiorosado.dev">FabioRosado</a>
-                    </div>
-                    <div className="social">
-                            <a href="https://github.com/FabioRosado"  aria-label="Link to Github Profile"><i className="fab fa-2x fa-github-square" />FabioRosado</a>
-                            <a href="https://twitter.com/FabioRosado_" aria-label="Link to Twitter Profile" rel="me"><i className="fab fa-2x fa-twitter-square" />FabioRosado_</a>
-                            <a href="https://twitch.tv/theflyingdev/" aria-label="Link to twitch profile"><i className="fab fa-2x fa-twitch"/>TheFlyingDev</a>
-                            <a href="https://www.instagram.com/FabioRosado/" aria-label="Link to Instagram Profile"><i className="fab fa-2x fa-instagram" />TheFlyingDev</a>
-                    </div>
-                </section>
+                    <a className="p-name u-url" href="https://fabiorosado.dev">FabioRosado</a>
+                </div>
+                <div className="social">
+                    <a href="https://github.com/FabioRosado" aria-label="Link to Github Profile"><i className="fab fa-2x fa-github-square" />FabioRosado</a>
+                    <a href="https://twitter.com/FabioRosado_" aria-label="Link to Twitter Profile" rel="me"><i className="fab fa-2x fa-twitter-square" />FabioRosado_</a>
+                    <a href="https://twitch.tv/theflyingdev/" aria-label="Link to twitch profile"><i className="fab fa-2x fa-twitch" />TheFlyingDev</a>
+                    <a href="https://www.instagram.com/FabioRosado/" aria-label="Link to Instagram Profile"><i className="fab fa-2x fa-instagram" />TheFlyingDev</a>
+                </div>
+            </section>
         </Layout>
     )
 }
