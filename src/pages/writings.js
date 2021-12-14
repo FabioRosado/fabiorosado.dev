92c28a26bb2cfd3fd261bc22793504c0e2ebd82d
FabioRosado <fabioglrosado@gmail.com>
2021-12-14 19:16:12 +0000

    Add buttons to filter posts by category

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 import Layout from '../components/layout'
@@ -6,27 +6,57 @@ import SEO from "../components/seo"
 import WritingCard from '../components/blog'
 
 
-class Blog extends Component {
-    render() {
-        const posts = this.props.data.blog
-        return(
-            <Layout>
-            <SEO title="FabioRosado | Writings" description="I write about code and share quick coding tips." />
-              <section className="blog-post">
-                <div className="post-header">
-                  <h1 className="large white-text">Writings</h1>
-                </div>
-                  <section className="page-container">
-                    {posts.nodes.map((post) => 
-                      <WritingCard post={post} key={post.frontmatter.title} />  
-                    )}
-                  </section>
-              
-              </section>
-            </Layout>
-        )
-    }
+const Blog = (props) => {
+
+  const allArticles = props.data.blog.nodes
+  const [articles, setArticles] = useState(allArticles)
+
+  const filterArticles = (category) => {
+
+    const filtered = allArticles.filter(article => {
+
+      if (article.frontmatter.categories == category) {
+        if (allArticles.length !== articles.length && !articles.includes(category)) {
+          return article
+        }
+        return article
+      }
+    })
+
+    setArticles(filtered)
+  }
+
+  console.log(articles)
+
+  const getAllArticles = () => {
+    setArticles(allArticles)
+  }
+  const posts = props.data.blog
+
+  return (
+    <Layout>
+      <SEO title="FabioRosado | Writings" description="I write about code and share quick coding tips." />
+      <section className="blog-post">
+        <div className="post-header">
+          <h1 className="large white-text">Writings</h1>
+          <div className="filter-search">
+            <button onClick={() => getAllArticles()} className="full-button small-margin-right margin-top">All</button>
+            {posts.allCategories.map(category =>
+              <button key={category.fieldValue} onClick={() => filterArticles(category.fieldValue)} className="full-button small-margin-right margin-top">
+                {category.fieldValue}
+              </button>
+            )}
+          </div>
+        </div>
+        <section className="page-container">
+          {articles.map(article => <WritingCard post={article} key={article.frontmatter.title} />)}
+        </section>
+
+      </section>
+    </Layout>
+  )
 }
+
 
 export default Blog
 
@@ -50,6 +80,9 @@ export const pageQuery = graphql`
           }
         }
       }
+    }
+    allCategories: group(field: frontmatter___categories) {
+      fieldValue
     }
   }
 }
