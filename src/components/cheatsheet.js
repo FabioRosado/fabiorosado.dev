9540add1fd6b3cc1042479ca0f5aadcbcae35621
FabioRosado <fabioglrosado@gmail.com>
2021-12-20 12:25:21 +0000

    Update cheatsheet cards

--- src/components/cheatsheet.js
+++ src/components/cheatsheet.js
@@ -7,17 +7,13 @@ import { GatsbyImage } from "gatsby-plugin-image"
 const CheatsheetCard = (props) => {
     const { frontmatter } = props.post
     return (
-        <div className="post-card" key={frontmatter.title}>
+        <div className="cheatsheet-card" key={frontmatter.title}>
             <Link to={`/cheatsheets/${frontmatter.slug}`}>
-                <div className="description cheatsheet">
-                    <p className="white-text small-font margin-top margin-left">{frontmatter.excerpt}</p>
-                    <span className="trans-button small-font">Read More <i className="fas fa-angle-double-right"></i></span>
-                </div>
                 <div className="card-title">
                     <span className="category"><i className={`${frontmatter.category_icon}`}></i> {frontmatter.categories}</span>
                     <h4 className="white-text">{frontmatter.title}</h4>
                 </div>
-                <GatsbyImage className="post-image" image={frontmatter.image.childImageSharp.gatsbyImageData} alt={frontmatter.title} />
+                <GatsbyImage className="image" image={frontmatter.image.childImageSharp.gatsbyImageData} alt={frontmatter.title} />
             </Link>
         </div>
     )
